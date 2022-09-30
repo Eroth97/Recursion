fbab5c1d7322493000ee3a02464408473b1838ec
David Tellez <davidctellezg@gmail.com>
2022-09-29 19:20:51 -0500

    Solve fibonacci problem

---
+++ Exercise_1/fibonaci.js
@@ -0,0 +1,21 @@
+//Non recursive version.
+function fibs(n){
+  let fibArray = [];
+  for (let i = 0; i < n; i++){
+    if (i === 0){
+      fibArray.push(0);
+    } else if(i === 1){
+      fibArray.push(1);
+    } else{
+      fibArray.push(fibArray[i-1] + fibArray[i-2]);
+    }
+  }
+  return fibArray;
+}
+
+//Recursive version.
+function fibsRec(n){
+  return n === 1? [0]: n === 2? [0, 1]: fibsRec(n-1).concat([fibsRec(n-1)[n-2] + fibsRec(n-1)[n-3]]);
+};
+console.log(fibs(8));
+console.log(fibsRec(8));
